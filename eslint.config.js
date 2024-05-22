614baa9bad470d889f13f8fa387d223bb816d2d7
Ersan Karimi <ersankarimii311@gmail.com>
2024-05-22 11:55:55 +0700

    style: Update eslint.config.js with new rules

--- eslint.config.js
+++ eslint.config.js
@@ -3,13 +3,13 @@ import antfu from "@antfu/eslint-config";
 export default antfu({
   formatters: true,
   vue: true,
-  rules: {
-    "semi": ["error", "always"],
-    "no-console": "warn",
-    "quotes": ["error", "double"],
-  },
   stylistic: {
     quotes: "double",
     semi: true,
+    indent: 2,
+  },
+  rules: {
+    "no-console": "warn",
+    "curly": ["error", "multi-line"],
   },
 });
